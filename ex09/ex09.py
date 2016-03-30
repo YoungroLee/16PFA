91ef2df8c4d5e374997557560896b5c7d986eedb
CAD Client <CAD Client>
2016-03-30 18:57:37 +0900

    #TODO(python에서 따로 관리해줌)  backslash = \ \키로 입력하면 backslash로 나온다.

--- ex09/ex09.py
+++ ex09/ex09.py
@@ -5,7 +5,7 @@ months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print "Here are the days: ", days
 print "Here are the months: ", months
-
+#TODO : explain backslash and \
 print """
 There's something going on here.
 With the three double-quotes.
